04c2320c5c5501e785ed27eac5555041b4ced6ad
kepa perez <kepa perez@DESKTOP-NOCEGND>
2020-04-21 10:07:34 +0200

    ApiUsuario

---
+++ src/controlador/ApiUsuario.java
@@ -0,0 +1,64 @@
+package controlador;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import modelo.bean.Usuario;
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class ApiUsuario
+ */
+@WebServlet("/ApiUsuario")
+public class ApiUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloUsuario mUsuario = new ModeloUsuario();
+		Usuario usuario = mUsuario.get(id);
+		
+		JSONObject jsonObject = new JSONObject(usuario);
+		String jsonString = jsonObject.toString();
+		
+	    PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+//		PrintWriter out = response.getWriter();
+
+		response.setHeader("Access-Control-Allow-Origin", "*"); // jsonp deia denean ez da behar
+		response.setContentType("application/json");	//erantzunaren MIME mota zein den zehazten du
+		response.setCharacterEncoding("UTF-8");			//kodifikazioa zehazten du
+
+		out.print(jsonString);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
