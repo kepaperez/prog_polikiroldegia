c0b5476d9e1c74d6bde6ae56d5d5cfd0be82843d
kepaperez <57668544+kepaperez@users.noreply.github.com>
2020-05-04 15:23:21 +0200

    Update Main.java

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -7,4 +7,33 @@ public class Main {
 
 	}
 
+dn: uid=jlopez,ou=usuarios,dc=somebooks,dc=local
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+uid: jlopez
+sn: Lopez
+givenName: Juan
+cn: Juan Lopez
+displayName: Juan Lopez
+uidNumber: 1000
+gidNumber: 10000
+userPassword: mi_password
+gecos: Juan Lopez
+
+loginShell: /bin/bash
+homeDirectory: /home/jlopez 
+shadowExpire: -1
+shadowFlag: 0
+shadowWarning: 7
+shadowMin: 8
+shadowMax: 999999
+shadowLastChange: 10877
+mail: juan.lopez@somebooks.com
+postalCode: 29000
+o: somebooks
+initials: JL
+	
+	
+	
 }
